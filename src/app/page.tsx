77a304b6b5732195bd92d5fb36b7cdfa3a9c6079
Nina711 <curbelonobleromina@gmail.com>
2025-12-17 22:44:07 -0300

    Improve style of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,35 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Streaver challenge app</h1>
-      <p>Here's what you can do with this app (click on either one to test):</p>
-      <ul>
-        <li><Link href="/posts">List posts</Link></li>
-        <li>Search posts by id</li>
-      </ul>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <div style={{ marginTop: "100px", textAlign: "center" }}>
+        <h1 style={{ fontSize: 32, marginBottom: 8 }}>
+          Streaver challenge app
+        </h1>
+
+        <p style={{ marginBottom: 24 }}>
+          This app allows users to list posts from an API and filter them by userId.
+        </p>
+
+        <Link
+          href="/posts"
+          style={{
+            display: "inline-block",
+            padding: "12px 20px",
+            backgroundColor: "#F875AA",
+            color: "white",
+            borderRadius: 8,
+          }}
+        >
+          Go to the posts
+        </Link>
+      </div>
     </main>
   );
 }
