9267cf49a9241375a962ec77b2441a00aee25f2c
Nina711 <curbelonobleromina@gmail.com>
2025-12-17 21:40:56 -0300

    Add posts list with swr loading. Change title and description of document

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,72 @@
-export default function PostsPage() {
-  return <h1>Posts page works</h1>;
+"use client";
+
+import useSWR from "swr";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function fetcher(url:string): Promise<Post[]>{
+  const res = await fetch(url);
+  if(!res.ok) throw new Error("Error fetching posts");
+  return res.json();
+} 
+
+export default function PostsPage(){
+  const{ data, error, isLoading, isValidating } = useSWR<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher,
+    {
+      revalidateOnReconnect: true,
+      shouldRetryOnError: true,
+    }
+  );
+
+  if(isLoading){
+    return (
+        <p style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center" }}>Loading posts...</p>
+  );
+  }
+
+  if(error){
+    return (
+      <p style = {{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        color: "crimson" }}>Error: {String(error.message || error)}</p>
+    );
+  }
+
+  return(
+    <main style = {{ maxWidth: 900, margin: "40px auto", padding: 16}}>
+      <h1 style = {{ fontSize:28, marginBottom:8 }}>Posts</h1>
+      <p style = {{ opacity: 0.7, marginTop: 0, marginBottom: "8px" }}>
+        {isValidating ? "Updating...": "Done"}
+      </p>
+
+      {data?.map((post) => (
+        <div key={post.id} style={{
+            border: "1px solid #e5e7eb",
+            borderRadius: 12,
+            padding: 16,
+            marginBottom: 12
+          }}>
+            <div style={{ fontSize: 12, opacity: 0.7 }}>
+              <p>User #{post.userId} - Post #{post.id}</p>
+            </div>
+            <h3 style = {{margin: "8px 0"}}>{post.title}</h3>
+            <p style = {{ margin: 0 }}>{post.body}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
